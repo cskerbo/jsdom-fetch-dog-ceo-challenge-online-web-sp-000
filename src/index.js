0ddf8edfeecf7c739bd7d3fc5c659297b003012a
Chris Kerbo <chris.kerbo@outlook.com>
2020-03-18 20:10:01 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -53,7 +53,10 @@ function dogPictures(json) {
 }
 
 const li = document.querySelectorAll('li')
-document.addEventListener("click")
+function changeLiColor() {
+  li.style.color = "red";
+}
+li.addEventListener("click", changeLiColor)
 
 document.addEventListener("DOMContentLoaded", function(){
   console.log('%c HI', 'color: firebrick');
