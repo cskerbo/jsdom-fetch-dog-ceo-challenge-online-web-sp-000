4e0426fa27d2b4497a758e7bc0346be184dcbe01
Chris Kerbo <chris.kerbo@outlook.com>
2020-03-18 19:17:06 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -28,7 +28,7 @@ function dogBreeds(json) {
   for (const key in breedsArray) {
     newLi = document.createElement('li')
     newLi.innerText = key
-    console.log(key[0])
+    console.log(key)
     breedContainer.appendChild(newLi)
   }
 }
