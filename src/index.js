d1c92ebaf0873ebc5cad8149e74775353a0dcf7d
Chris Kerbo <chris.kerbo@outlook.com>
2020-03-18 20:13:00 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -52,7 +52,7 @@ function dogPictures(json) {
   }
 }
 
-const li = document.getElementsByTag('li')
+const li = document.getElementsByTagName('li')
 function changeLiColor() {
   li.style.color = "red";
 }
