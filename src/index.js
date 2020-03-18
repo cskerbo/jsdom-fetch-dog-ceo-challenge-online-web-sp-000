c457c06d160e6512c1bc4a6371b5c54ce7a35bed
Chris Kerbo <chris.kerbo@outlook.com>
2020-03-18 19:13:35 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -28,7 +28,7 @@ function dogBreeds(json) {
   for (const key in breedsArray) {
     newLi = document.createElement('li')
     newLi.innerText = key
-    for (key[0]) {
+    for key[0] {
       newUl = document.createElement('ul')
       newUl.innerText = element
       newLi.appendChild(newUl)
