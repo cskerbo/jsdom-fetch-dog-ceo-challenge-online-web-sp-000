6dbea24c5681d1cb6d97ca2a41263aac15294979
Chris Kerbo <chris.kerbo@outlook.com>
2020-03-18 20:31:38 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -64,6 +64,8 @@ document.addEventListener("DOMContentLoaded", function(){
   dogUL.addEventListener("click", function(event){
       if (event.target.dataset.info === "breed") {
         event.target.style.color = "green"
+      else if (event.target.dataset.info === "sub-breed")
+        event.target.style.color = "red"
       }
     })
 });
