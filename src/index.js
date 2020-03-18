2c2ea90f8fb6da874af02e07beb540b487e94700
Chris Kerbo <chris.kerbo@outlook.com>
2020-03-18 18:06:10 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 console.log('%c HI', 'color: firebrick')
 const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
-let imgContainer = document.getElementById('dog-image-container');
+let imgContainer = document.querySelectorAll('dog-image-container');
 
 function fetchDogs() {
   fetch(imgUrl)
