63ae4922b89a80e5223e39697010e08b6c79147f
Chris Kerbo <chris.kerbo@outlook.com>
2020-03-18 17:29:18 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
 console.log('%c HI', 'color: firebrick')
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+
+function fetchDogs() {
+  fetch(imgUrl)
+  .then(function(response) {
+  return response.json();
+})
+.then(function(json) {
+  dogPictures(json)
+});
+}
+
+function dogPictures(json) {
+  pictureArray = [json.message]
+  return pictureArray[1]
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+
+});
